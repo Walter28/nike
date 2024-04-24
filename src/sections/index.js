71b69b51ca697097738837847d08ddea425d0747
Walter28 <walterchristian28@gmail.com>
2024-04-24 09:51:10 +0200

    creation of src/sections/index.js file to make import properly

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularOffer from "./PopularOffer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Suscribe from "./Suscribe";
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularOffer,
+    PopularProducts,
+    Services,
+    SuperQuality,
+    Suscribe
+}
